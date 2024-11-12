686761cecbbb57835cf8f3d4d621dd3115934a77
areum0116 <areum0116@naver.com>
2024-11-12 20:36:51 +0900

    init

---
+++ src/main/java/jpabook/auctiongateway/AuctionGatewayApplication.java
@@ -0,0 +1,13 @@
+package jpabook.auctiongateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuctionGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuctionGatewayApplication.class, args);
+    }
+
+}
