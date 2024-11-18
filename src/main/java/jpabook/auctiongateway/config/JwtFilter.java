e90d51edc2438b6a9d3f74ece1a6715e9bbb4598
MOON <moonjs1018@naver.com>
2024-11-18 17:03:23 +0900

    hotfix : header 설정 변경

--- src/main/java/jpabook/auctiongateway/config/JwtFilter.java
+++ src/main/java/jpabook/auctiongateway/config/JwtFilter.java
@@ -8,13 +8,15 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.http.server.reactive.ServerHttpRequestDecorator;
-import org.springframework.http.server.reactive.ServerHttpResponse;
-import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.ReactiveSecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import org.springframework.web.server.WebFilter;
 import org.springframework.web.server.WebFilterChain;
 import reactor.core.publisher.Mono;
+
+import java.util.Collections;
 
 @Slf4j
 @Component
@@ -25,26 +27,17 @@ public class JwtFilter implements WebFilter {
 
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
-        ServerHttpRequest request = exchange.getRequest();
-        String path = request.getURI().getPath();
+        String path = exchange.getRequest().getURI().getPath();
+
         log.info("request path: {}", path);
 
-        // 인증 X
-        if (path.startsWith("/api/v1/auth/")
-                || path.equals("/error")
-                || path.equals("/style.css")
-                || path.startsWith("/api/v2/auctions/search")
-                || path.startsWith("/api/v2/auctions/elasticsearch")
-                || path.equals("/actuator/health")
-                || path.equals("/health")
-                || path.equals("/actuator/prometheus")
-                || path.equals("/api/v1/points/buy/confirm")) {
+        // 인증이 필요 없는 경로 필터링
+        if (isExcludedPath(path)) {
             return chain.filter(exchange);
         }
 
-        String authorizationHeader = request.getHeaders().getFirst(HttpHeaders.AUTHORIZATION);
-        if(authorizationHeader == null || !authorizationHeader.startsWith("Bearer ")) {
-//            return chain.filter(exchange);
+        String authorizationHeader = exchange.getRequest().getHeaders().getFirst(HttpHeaders.AUTHORIZATION);
+        if (authorizationHeader == null || !authorizationHeader.startsWith("Bearer ")) {
             return onError(exchange, "No Authorization Header Found", HttpStatus.UNAUTHORIZED);
         }
 
@@ -57,37 +50,51 @@ public class JwtFilter implements WebFilter {
 
             Long userId = Long.valueOf(claims.getSubject());
 
-            if (userId != null && SecurityContextHolder.getContext().getAuthentication() == null) {
-                ServerHttpRequest decoratedRequest = new ServerHttpRequestDecorator(request) {
+            if (userId != null) {
+                UsernamePasswordAuthenticationToken authentication =
+                        new UsernamePasswordAuthenticationToken(userId, null, null);
+
+                ServerHttpRequest mutatedRequest = new ServerHttpRequestDecorator(exchange.getRequest()) {
                     @Override
                     public HttpHeaders getHeaders() {
                         HttpHeaders headers = new HttpHeaders();
                         headers.putAll(super.getHeaders());
-                        headers.add("userId", Long.toString(userId));
+                        headers.put("userId", Collections.singletonList(Long.toString(userId)));
+                        headers.set(HttpHeaders.AUTHORIZATION, authorizationHeader);
                         return headers;
                     }
                 };
 
                 ServerWebExchange mutatedExchange = exchange.mutate()
-                        .request(decoratedRequest)
+                        .request(mutatedRequest)
                         .build();
 
-                return chain.filter(mutatedExchange);
+                return chain.filter(mutatedExchange)
+                        .contextWrite(ReactiveSecurityContextHolder.withAuthentication(authentication));
             } else {
                 return onError(exchange, "Invalid user ID", HttpStatus.UNAUTHORIZED);
             }
-
         } catch (Exception e) {
+            log.error("Error while processing JWT: ", e);
             return onError(exchange, e.getMessage(), HttpStatus.UNAUTHORIZED);
         }
     }
 
-    private Mono<Void> onError(ServerWebExchange exchange, String err, HttpStatus httpStatus) {
-        ServerHttpResponse response = exchange.getResponse();
-        response.setStatusCode(httpStatus);
+    private boolean isExcludedPath(String path) {
+        return path.startsWith("/api/v1/auth/") ||
+                path.equals("/error") ||
+                path.equals("/style.css") ||
+                path.startsWith("/api/v2/auctions/search") ||
+                path.startsWith("/api/v2/auctions/elasticsearch") ||
+                path.equals("/actuator/health") ||
+                path.equals("/health") ||
+                path.equals("/actuator/prometheus") ||
+                path.equals("/api/v1/points/buy/confirm");
+    }
 
+    private Mono<Void> onError(ServerWebExchange exchange, String err, HttpStatus httpStatus) {
         log.error(err);
-        return response.setComplete();
+        exchange.getResponse().setStatusCode(httpStatus);
+        return exchange.getResponse().setComplete();
     }
 }
-
